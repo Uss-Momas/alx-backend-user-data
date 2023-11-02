b8122145bcf3a4604afa60fedc470c2996832884
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-11-02 08:26:51 +0200

    Ref filter_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
 filtered_logger module
+To handle personal data
 """
 from typing import List
 from re import sub
@@ -8,17 +9,7 @@ from re import sub
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """
-    filter_datum function
-    Args:
-        - fields: list of strings representing all fields to obfuscate
-        - redaction: string representing by what the field will be obfuscated
-        - message: string representing the log line
-        - separator: string representing by which character is separating all
-        fields in the log line (message)
-    Returns:
-        - log message obfuscated
-    """
+    """filter_datum function retuns a obsfucated log message"""
     for field in fields:
         replicate = f"{field}={redaction}{separator}"
         message = sub(f"{field}=.*?{separator}", replicate, message)
