86b14ee5c72dc295e77284238399a63e2c42d288
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-11-02 14:12:30 +0200

    Implement a function that chechs if a password matches a hash

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -15,3 +15,15 @@ def hash_password(password: str) -> bytes:
     """
     hashed = bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    is_valid function expects 2 arguments and returns a boolean.
+    Args:
+        - hashed_password: bytes type
+        - password: string type
+    Return:
+        - boolean value indicating if password is valida or not
+    """
+    return bcrypt.checkpw(bytes(password, 'utf-8'), hashed_password)
