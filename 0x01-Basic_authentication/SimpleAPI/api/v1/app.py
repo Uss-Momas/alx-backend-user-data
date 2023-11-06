6e501fdf7b9600a0b5e61e36813e779eb70e09ab
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-11-06 17:01:36 +0200

    uncomment line

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -18,8 +18,8 @@ auth = None
 if os.getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-# elif os.getenv("AUTH_TYPE") == "basic_auth":
-elif True:
+# elif True:
+elif os.getenv("AUTH_TYPE") == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
