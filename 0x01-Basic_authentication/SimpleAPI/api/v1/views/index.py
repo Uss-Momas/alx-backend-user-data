cd3ddefa3edba7ea0edf2ea4226e8ba863ed6d4e
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-11-06 11:18:29 +0200

    Add unauthorized route as example

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route("/unauthorized", strict_slashes=False)
+def unauthorized():
+    """aborts with 401 to trigger the unauthorized error handler
+    """
+    abort(401)
