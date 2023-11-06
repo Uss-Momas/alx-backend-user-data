abf40f4c03a0adc37e6f28c660f41a72c8b0e832
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-11-06 15:32:01 +0200

    Implement method to decode the bas64 string

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 used for basic authentication
 """
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -30,3 +32,23 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """decode_base64_authorization_header method
+        Args:
+            - base64_authorization_header: string containing the Authorizarion
+            with the basic Auth token in base 64
+        Returns:
+            - returns the decoded value of a Base64 string
+            base64_authorization_header
+        """
+        if not base64_authorization_header:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            base64_bytes = base64.b64decode(base64_authorization_header)
+        except binascii.Error:
+            return None
+        return base64_bytes.decode('utf-8')
