aa6074280631f679ae985f6ff03d9b3e0e9f7f2e
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-11-08 15:49:07 +0200

    Implement create session method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """session_auth module
 used for session authentication
 """
+from uuid import uuid4
 from api.v1.auth.auth import Auth
 
 
@@ -10,4 +11,19 @@ class SessionAuth(Auth):
     Class responsible for Session Authentication Example
     """
     def __init__(self) -> None:
+        """Constructor of the class"""
         super().__init__()
+        self.user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """create_session method
+        Args:
+            - user_id: string containing the id of the user
+        return:
+            - uuid4 in string format
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
