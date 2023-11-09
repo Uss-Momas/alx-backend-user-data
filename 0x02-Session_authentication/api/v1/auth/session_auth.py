fced614f49f24aa6b8d14f2476bc97794c382e1a
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-11-09 15:41:43 +0200

    Retrieve current user by session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -41,3 +41,15 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """current_user method overloaded from Auth
+        Args:
+            - request
+        Returns:
+            - current user found
+        """
+        from models.user import User
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
