4dba8e21e12e9b6314e96430e12c84b09f7b43cd
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-11-13 15:13:41 +0200

    Implement funcction to find user

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.exc import NoResultFound
+# from sqlalchemy.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance"""
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """add user method
+        Args:
+            - user email
+            - user hashed password
+        """
+        new_session = self._session
+        user = User()
+        user.email = email
+        user.hashed_password = hashed_password
+        new_session.add(user)
+        new_session.commit()
+        return user
+
+    def find_user_by(self, **kwargs: dict) -> User:
+        """Find a specific user
+        Args:
+            - kwargs: key named values dictionaires
+        """
+        session = self._session
+        for key, value in kwargs.items():
+            if hasattr(User, key) is None:
+                raise InvalidRequestError
+        user = session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
