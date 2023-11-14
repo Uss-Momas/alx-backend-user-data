bb9a464c0e436d6d0874bae86fc4e2693eb08409
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-11-14 12:01:15 +0200

    remove kwargs type

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -63,7 +63,7 @@ class DB:
             raise NoResultFound
         return user
 
-    def update_user(self, user_id: int, **kwargs: dict) -> None:
+    def update_user(self, user_id: int, **kwargs) -> None:
         """update user method
         Args:
             - user_id
