308804cf0d9a4e8b5516e447b587baf52b29b7d2
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-11-14 17:42:47 +0200

    Update password of user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,3 +86,13 @@ class Auth:
         uuid_gen = _generate_uuid()
         self._db.update_user(user.id, reset_token=uuid_gen)
         return uuid_gen
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """update_password method
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        pwd = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=pwd, reset_token=None)
