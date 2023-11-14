408a6e4fa1e08f035fb37058908560c9ad8e3922
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-11-14 12:26:32 +0200

    implement hash password function

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""auth module
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """function to hash password
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
